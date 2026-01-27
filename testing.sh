86806364a0498e46da35ebb8a7574fc9e0105e05
yiming <yiming33@students.ubc.ca>
2026-01-27 15:12:30 -0800

    add testing command

--- testing.sh
+++ testing.sh
@@ -1,24 +1,24 @@
-# python run_main.py \
-#     --rollout_type batched \
-#     --meta_model gpt-5 \
-#     --execution_model gpt-5-nano/low \
-#     --batch_max_update_concurrent 10 \
-#     --batch_max_retrieve_concurrent 10 \
-#     --task_type alfworld \
-#     --status eval_in_distribution \
-#     --memo_SHA 53cee295
+python run_main.py \
+    --rollout_type batched \
+    --meta_model gpt-5 \
+    --execution_model gpt-5-nano/low \
+    --batch_max_update_concurrent 10 \
+    --batch_max_retrieve_concurrent 10 \
+    --task_type alfworld \
+    --status eval_in_distribution \
+    --memo_SHA 53cee295
 
-# python run_main.py \
-#     --rollout_type sequential \
-#     --meta_model gpt-5 \
-#     --execution_model gpt-5-nano/low \
-#     --batch_max_update_concurrent 10 \
-#     --batch_max_retrieve_concurrent 10 \
-#     --task_type alfworld \
-#     --status eval_out_of_distribution \
-#     --update_task eval_in_distribution \
-#     --update_size 70 \
-#     --memo_SHA 53cee295
+python run_main.py \
+    --rollout_type sequential \
+    --meta_model gpt-5 \
+    --execution_model gpt-5-nano/low \
+    --batch_max_update_concurrent 10 \
+    --batch_max_retrieve_concurrent 10 \
+    --task_type alfworld \
+    --status eval_out_of_distribution \
+    --update_task eval_in_distribution \
+    --update_size 70 \
+    --memo_SHA 53cee295
 
 python run_main.py \
     --rollout_type batched \
@@ -30,22 +30,22 @@ python run_main.py \
     --status eval_in_distribution \
     --memo_SHA 70430b60
 
-# python run_main.py \
-#     --rollout_type batched \
-#     --meta_model gpt-5 \
-#     --execution_model gpt-5-nano/low \
-#     --batch_max_update_concurrent 10 \
-#     --batch_max_retrieve_concurrent 10 \
-#     --task_type babaisai \
-#     --status eval_in_distribution \
-#     --memo_SHA 7e79483e
+python run_main.py \
+    --rollout_type batched \
+    --meta_model gpt-5 \
+    --execution_model gpt-5-nano/low \
+    --batch_max_update_concurrent 10 \
+    --batch_max_retrieve_concurrent 10 \
+    --task_type babaisai \
+    --status eval_in_distribution \
+    --memo_SHA 7e79483e
 
-# python run_main.py \
-#     --rollout_type batched \
-#     --meta_model gpt-5 \
-#     --execution_model gpt-5-nano/low \
-#     --batch_max_update_concurrent 10 \
-#     --batch_max_retrieve_concurrent 10 \
-#     --task_type minihack \
-#     --status eval_in_distribution \
-#     --memo_SHA 0892408e
+python run_main.py \
+    --rollout_type batched \
+    --meta_model gpt-5 \
+    --execution_model gpt-5-nano/low \
+    --batch_max_update_concurrent 10 \
+    --batch_max_retrieve_concurrent 10 \
+    --task_type minihack \
+    --status eval_in_distribution \
+    --memo_SHA 0892408e
